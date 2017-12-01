33a9388682678ea06edf218b21713bac83de2639
Sam Waller <samuellwaller@gmail.com>
2017-12-01 02:49:35 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import React from 'react'
+import { render } from 'react-dom'
+import './css/style.css'
+import StorePicker from './components/StorePicker'
+
+render(<StorePicker />, document.querySelector("#main"))
