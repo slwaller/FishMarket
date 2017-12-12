c2f3217ec940f5c9fd6c1f630da08530931aceab
Sam Waller <samuellwaller@gmail.com>
2017-12-12 14:08:45 -0500

    setting up input to grab data

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,11 +3,17 @@ import React from 'react'
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+
+  goToStore(event){
+    event.preventDefault()
+
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+          <input type="text" required placeholder="Store Name" ref={(input) => {this.storeInput = input}} defaultValue={getFunName()}/>
           <button type="submit">Visit Store </button>
       </form>
     )
