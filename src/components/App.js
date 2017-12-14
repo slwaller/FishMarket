ef1969ae597c991b4360d78e31154ad0017398cb
Sam Waller <samuellwaller@gmail.com>
2017-12-14 05:04:09 -0500

    setting up fish state

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,24 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
+
+  constructor(){
+    super()
+
+    this.addFish = this.addFish.bind(this)
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish){
+    const fishes = {...this.state.fishes}
+    const timestamp = Date.now()
+    fishes[`fish-${timestamp}`] = fish
+    this.setState({ fishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -15,7 +33,9 @@ class App extends React.Component {
           />
         </div>
         <Order />
-        <Inventory />
+        <Inventory
+          addFish={this.addFish}
+        />
       </div>
     )
   }
