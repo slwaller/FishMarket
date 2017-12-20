29cf0dee1eba1a021ff76e362f00c92cc0628b96
Sam Waller <samuellwaller@gmail.com>
2017-12-20 00:53:52 -0500

    tracking order and displaying it

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,9 +2,31 @@ import React from 'react'
 import { formatPrice } from '../helpers'
 
 class Order extends React.Component {
+  constructor(){
+    super()
+    this.renderOrder = this.renderOrder.bind(this)
+  }
+  
+  renderOrder(key){
+    const fish = this.props.fishes[key]
+    const count = this.props.order[key]
+
+    if(!fish || fish.status === 'unavailable'){
+      return <li key={key}>Sorry, {fish ? fish.name : 'fish'} is no longer available</li>
+    }
+
+    return (
+      <li key={key}>
+        <span>{count}lbs {fish.name}</span>
+        <span className="price">{formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  }
+
   render(){
 
     const orderIds = Object.keys(this.props.order)
+
     const total = orderIds.reduce((prevTotal, key) => {
       const fish = this.props.fishes[key]
       const count = this.props.order[key]
@@ -19,7 +41,13 @@ class Order extends React.Component {
     return (
       <div>
         <h2>Your Order</h2>
-        <p>{formatPrice(total)}</p>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+          <li className="total">
+            <strong>Total:</strong>
+            {formatPrice(total)}
+          </li>
+        </ul>
       </div>
     )
   }
