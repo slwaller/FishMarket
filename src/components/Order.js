4e8868c3039a381cbd85bc485858bcf79e52e986
Sam Waller <samuellwaller@gmail.com>
2017-12-20 00:44:22 -0500

    real time tracking price

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { formatPrice } from '../helpers'
 
 class Order extends React.Component {
   render(){
+
+    const orderIds = Object.keys(this.props.order)
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key]
+      const count = this.props.order[key]
+      const isAvailable = fish && fish.status === 'available'
+
+      if(isAvailable){
+        return prevTotal + (count * fish.price || 0)
+      }
+      return prevTotal
+    }, 0)
+
     return (
-      <p>Order</p>
+      <div>
+        <h2>Your Order</h2>
+        <p>{formatPrice(total)}</p>
+      </div>
     )
   }
 }
