1778eda2e3e8847370a022b7a0d8c758c3d1b375
Sam Waller <samuellwaller@gmail.com>
2017-12-24 05:49:37 -0500

    keeping track of the order through localstorage

--- src/components/App.js
+++ src/components/App.js
@@ -27,10 +27,21 @@ class App extends React.Component {
       context: this,
       state: 'fishes'
     })
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`)
+
+    if(localStorageRef){
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
+    }
   }
 
   componentWillUnmount(){
     base.removeBinding(this.ref)
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order))
   }
 
   addFish(fish){
@@ -72,6 +83,7 @@ class App extends React.Component {
         <Order
           fishes={this.state.fishes}
           order={this.state.order}
+          params={this.props.params}
         />
         <Inventory
           addFish={this.addFish}
