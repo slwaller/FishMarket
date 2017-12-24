c2438ecb32bf90c97da954cef45b0edc9231bd72
Sam Waller <samuellwaller@gmail.com>
2017-12-24 05:25:49 -0500

    getting started with firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAcu1aPvDiDXszStanloypRPpTpX5maBCo",
+  authDomain: "fish-market-da933.firebaseapp.com",
+  databaseURL: "https://fish-market-da933.firebaseio.com",
+})
+
+export default base
