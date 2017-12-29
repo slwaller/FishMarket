7cc9235ba3f546a2c90f996fa9b4fc4a9d283783
Sam Waller <samuellwaller@gmail.com>
2017-12-28 23:32:13 -0500

    loading new images since old ones died

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -2,7 +2,7 @@
 module.exports = {
   fish1: {
     name: 'Pacific Halibut',
-    image: 'http://i.istockimg.com/file_thumbview_approve/36248396/5/stock-photo-36248396-blackened-cajun-sea-bass.jpg',
+    image: 'https://c1.staticflickr.com/3/2002/2221620159_0cef36a563_z.jpg?zz=1',
     desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
     price: 1724,
     status: 'available'
@@ -10,7 +10,7 @@ module.exports = {
 
   fish2: {
     name: 'Lobster',
-    image: 'http://i.istockimg.com/file_thumbview_approve/32135274/5/stock-photo-32135274-cooked-lobster.jpg',
+    image: 'https://cdn.pixabay.com/photo/2016/03/02/13/50/seafood-platter-1232389_1280.jpg',
     desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
     price: 3200,
     status: 'available'
@@ -26,7 +26,7 @@ module.exports = {
 
   fish4: {
     name: 'Mahi Mahi',
-    image: 'http://i.istockimg.com/file_thumbview_approve/12556651/5/stock-photo-12556651-mahimahi.jpg',
+    image: 'https://c1.staticflickr.com/1/42/86855835_c01f42131f_z.jpg?zz=1',
     desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
     price: 1129,
     status: 'available'
@@ -34,7 +34,7 @@ module.exports = {
 
   fish5: {
     name: 'King Crab',
-    image: 'http://i.istockimg.com/file_thumbview_approve/18294110/5/stock-photo-18294110-king-crab-legs.jpg',
+    image: 'https://c1.staticflickr.com/4/3013/3016455315_8e345c6bf6_b.jpg',
     desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces',
     price: 4234,
     status: 'available'
@@ -42,7 +42,7 @@ module.exports = {
 
   fish6: {
     name: 'Atlantic Salmon',
-    image: 'http://i.istockimg.com/file_thumbview_approve/56241842/5/stock-photo-56241842-salmon-fish.jpg',
+    image: 'https://cdn.pixabay.com/photo/2014/12/08/15/07/salmon-560987_1280.jpg',
     desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
     price: 1453,
     status: 'available'
@@ -50,7 +50,7 @@ module.exports = {
 
   fish7: {
     name: 'Oysters',
-    image: 'http://i.istockimg.com/file_thumbview_approve/58626682/5/stock-photo-58626682-fresh-oysters-on-a-black-stone-plate-top-view.jpg',
+    image: 'http://maxpixel.freegreatpicture.com/static/photo/2x/Meal-Japanese-Izakaya-Food-Oysters-Foods-Cuisine-220955.jpg',
     desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
     price: 2543,
     status: 'available'
@@ -58,7 +58,7 @@ module.exports = {
 
   fish8: {
     name: 'Mussels',
-    image: 'http://i.istockimg.com/file_thumbview_approve/40450406/5/stock-photo-40450406-steamed-mussels.jpg',
+    image: 'https://cdn.pixabay.com/photo/2017/09/03/17/12/sauteed-mussels-2711241_1280.jpg',
     desc: 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
     price: 425,
     status: 'available'
@@ -66,7 +66,7 @@ module.exports = {
 
   fish9: {
     name: 'Jumbo Prawns',
-    image: 'http://i.istockimg.com/file_thumbview_approve/67121439/5/stock-photo-67121439-fresh-tiger-shrimp-on-ice-on-a-black-stone-table.jpg',
+    image: 'http://maxpixel.freegreatpicture.com/static/photo/2x/Shrimp-Food-Sea-Delicious-Cooked-Prawn-Seafood-1743374.jpg',
     desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
     price: 2250,
     status: 'available'
